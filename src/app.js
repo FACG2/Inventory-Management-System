f856ff159d2b5687218407a0b70ddbda87944019
samer elaila <samer-elaila@live.com>
2017-09-19 13:39:40 +0300

    add project basic structure

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+const path = require('path');
+const routes = require('./controllers/index');
+const helmet = require('helmet');
+
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'hbs');
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.engine('hbs', expressHandlebars({
+  extname: 'hbs',
+  layoutsDir: path.join('views', 'layouts'),
+  partialsDir: path.join('views', 'partials'),
+  defaultLayout: 'main'
+}));
+
+app.use(routes);
+
+app.set('PORT', 3000);
+
+module.exports = app;
