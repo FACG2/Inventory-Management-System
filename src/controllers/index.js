e42b95e2f0f14088eff9797f01ddefb7d90d1907
samer elaila <samer-elaila@live.com>
2017-09-20 15:16:38 +0300

    app signin and sign up routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,10 +4,14 @@ const landing = require('./landing');
 // const db = require('../models/db_functions/index');
 // const signup = require('../middlewares/sign_up.js');
 const profile = require('./profile');
-const signup = require('./signup');
+const authController = require('./auth');
 
 router.get('/', landing.get);
 router.get('/profile', profile.get);
-router.post('/signup', signup.post);
+router.post('/sign-up', authController.signup);
+// router.post('/signin', authController.login);
+router.get('/home', (req, res) => {
+  res.send('Home Page!!');
+});
 
 module.exports = router;
