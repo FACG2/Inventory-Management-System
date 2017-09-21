bb5f4081095f20cf7018c4e0609ff57723f12045
samer elaila <samer-elaila@live.com>
2017-09-21 08:24:27 +0300

    add sign-in and logout routes and add auth middleware to secure some views

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,13 +5,24 @@ const landing = require('./landing');
 // const signup = require('../middlewares/sign_up.js');
 const profile = require('./profile');
 const authController = require('./auth');
+const authMiddleware = require('../middlewares/auth');
 
-router.get('/', landing.get);
-router.get('/profile', profile.get);
+router.get('/', authMiddleware.checkAuth, landing.get);
+router.get('/profile', authMiddleware.checkAuth, profile.get);
 router.post('/sign-up', authController.signup);
-// router.post('/signin', authController.login);
-router.get('/home', (req, res) => {
+router.post('/sign-in', authController.signIn);
+router.get('/logout', authController.logout);
+
+router.get('/home', authMiddleware.checkAuth, (req, res) => {
   res.send('Home Page!!');
+});
+
+router.get('/test', authMiddleware.checkAuth, (req, res) => {
+  res.send('secret route');
+});
+
+router.get('*', (req, res) => {
+  res.send('404 Page Not Found!!!');
 });
 
 module.exports = router;
