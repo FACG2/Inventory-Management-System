08de82d42dab359cd08f1fe244a539c653f37e8a
samer elaila <samer-elaila@live.com>
2017-09-21 08:25:19 +0300

    fix jwt.verify function by adding SECRET variable to it =_=

--- src/helpers/verifyToken.js
+++ src/helpers/verifyToken.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 require('env2')('config.env');
 
 const verifyToken = (token, cb) => {
-  jwt.verify(token, (err, decoded) => {
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
       cb(err);
     } else {
