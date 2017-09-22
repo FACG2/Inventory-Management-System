6cae78d20e21364f6c4551075797a3fde684f42e
Salwa <herotony111@gmail.com>
2017-09-22 14:16:08 +0300

    change error style

---
+++ src/controllers/addGoods.js
@@ -0,0 +1,26 @@
+const queries = require('../models/db_functions/goodsFunctions.js');
+
+function get (req, res) {
+  res.render('inventory', {title: 'Add Goods', cssPath: '/css/inventory.css'});
+}
+
+function post (req, res, next) {
+  const Data = {
+    body: req.bod
+    // params: req.params
+  };
+
+  queries.addGoods(Data, (err, result) => {
+    if (err) {
+      console.log(err);
+      next(err);
+    } else {
+      res.redirect('/home');
+    }
+  });
+}
+
+module.exports = {
+  get,
+  post
+};
