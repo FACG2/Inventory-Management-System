b5dccd1e1289dcced51780723c82a6a74fca29cb
samer elaila <samer-elaila@live.com>
2017-09-26 16:22:10 +0300

    add addInventory Controller

---
+++ src/controllers/addInventory.js
@@ -0,0 +1,28 @@
+const db = require('../models/db_functions/index');
+
+const get = (req, res, next) => {
+  res.render('inventoryPage');
+};
+
+const post = (req, res, next) => {
+  const data = {
+    name: req.body.name,
+    location: req.body.location,
+    capacity: 300,
+    status: 'فارغ',
+    user_id: req.user.id
+  };
+  db.Inventories.addInventory(data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/500');
+    } else {
+      res.redirect('/home');
+    }
+  });
+};
+
+module.exports = {
+  get,
+  post
+};
