53f3f19071a1b53726ada0919d90149d6a03d93c
samer elaila <samer-elaila@live.com>
2017-09-26 17:51:13 +0300

    remove commented lines

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -1,39 +1,21 @@
 const db = require('../models/db_functions/index');
 
 const get = (req, res, next) => {
-  db.Reports.getTransactionsForInventory(1, (err, result) => {
+  db.Reports.getTransactionsForInventory(req.user.id, (err, result) => {
     if (err) {
       console.log(err);
       next(err);
     } else {
       let counter = 1;
       result = result.map((item) => {
-        // console.log(item);
         item.number = counter++;
         return item;
       });
       const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-      // const
-      // console.log(result);
-      // const years = getYears();
-      // console.log(years);
       res.render('report', {result, days});
     }
   });
 };
-
-// const getYears = () => {
-//   const date = new Date();
-//   const currentYear = date.getFullYear();
-//   const years = date.getFullYear() - 1970;
-//   var res = [];
-//   const arr = new Array(years);
-//   arr.map((year, index) => {
-//     res.push(currentYear + (index + 1));
-//   });
-//
-//   console.log(res.length);
-// };
 
 module.exports = {
   get
