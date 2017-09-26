7a3a5f06c1219d121938f90db004006f6f545365
samer elaila <samer-elaila@live.com>
2017-09-26 11:17:52 +0300

    add graph helper

---
+++ src/helpers/graph.js
@@ -0,0 +1,24 @@
+const db = require('../models/db_functions/index');
+
+// get all transactions and filter them and return a format to return it back.
+const filterTransactions = (cb) => {
+  db.Transactions.getAll((err, result) => {
+    if (err) {
+      cb(err);
+    } else {
+      const output = {};
+      result.map((row) => {
+        if (output.hasOwnProperty(row.good_name)) {
+          output[row.good_name] += parseInt(row.quantity);
+        } else {
+          output[row.good_name] = row.quantity;
+        }
+      });
+      cb(null, output);
+    }
+  });
+};
+
+module.exports = {
+  filterTransactions
+};
