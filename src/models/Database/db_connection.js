212958b98c0953a83d0d598eb29c2f6a97c08d68
samer elaila <samer-elaila@live.com>
2017-10-01 16:18:47 +0300

    solve conflict

--- src/models/Database/db_connection.js
+++ src/models/Database/db_connection.js
@@ -2,10 +2,10 @@ const {Pool} = require('pg');
 require('env2')('./config.env');
 let dataUrl;
 
-if (!process.env.DATABASE_URL) {
+process.env.NODE_ENV === 'test' ? dataUrl = process.env.TEST_URL : dataUrl = process.env.DATABASE_URL;
+
+if (!dataUrl) {
   throw new Error('No DATABASE_URL provided');
 }
-
-process.env.NODE_ENV === 'test' ? dataUrl = process.env.TEST_URL : dataUrl = process.env.DATABASE_URL;
 
 module.exports = new Pool({connectionString: dataUrl, ssl: true});
